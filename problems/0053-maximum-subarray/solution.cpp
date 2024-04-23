dac6c7f60d712fe38a736c9b9eddf1ec037fb459
Kerwin Tsai <kerwintsai@pm.me>
2024-04-24 05:16:51 +0800

    Finish 53 Maximum Subarray

--- problems/0053-maximum-subarray/solution.cpp
+++ problems/0053-maximum-subarray/solution.cpp
@@ -1,14 +1,17 @@
-class Solution {
+class Solution
+{
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int> &nums)
+    {
         int max_global = nums[0];
         int max_current = nums[0];
 
-        for(int i = 1; i < nums.size(); i++) {
-            max_current = max(nums[i], max_current+nums[i]);
-            if(max_current > max_global)
+        for (int i = 1; i < nums.size(); i++)
+        {
+            max_current = max(nums[i], max_current + nums[i]);
+            if (max_current > max_global)
                 max_global = max_current;
-        } 
+        }
         return max_global;
     }
 };
