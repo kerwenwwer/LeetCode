c2fa0566cee04d8d089b9b1b927ebe63192b585a
Kerwin Tsai <kerwintsai@pm.me>
2024-04-24 17:18:51 +0800

    Finish 149 Reorder List

--- problems/0149-reorder-list/solution.cpp
+++ problems/0149-reorder-list/solution.cpp
@@ -8,34 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    void reorderList(ListNode* head) {
+    void reorderList(ListNode *head)
+    {
         // Find the middle element of the list
         ListNode *slow = head;
         ListNode *fast = head;
-        while(fast->next && fast->next->next){
+        while (fast->next && fast->next->next)
+        {
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        //reverse the second half
+        // reverse the second half
         ListNode *prev = nullptr;
         ListNode *curr = slow->next;
-        while(curr){
+        while (curr)
+        {
             ListNode *temp = curr->next;
             curr->next = prev;
             prev = curr;
             curr = temp;
         }
-        slow->next = nullptr;   //split to two lists
-        
-        ListNode* first = head; // first half starts from head
-        ListNode* second = prev; // second half starts from prev (reversed list's head)
+        slow->next = nullptr; // split to two lists
 
-        while(second){
-            ListNode* temp1 = first->next;
-            ListNode* temp2 = second->next;
+        ListNode *first = head;  // first half starts from head
+        ListNode *second = prev; // second half starts from prev (reversed list's head)
+
+        while (second)
+        {
+            ListNode *temp1 = first->next;
+            ListNode *temp2 = second->next;
 
             first->next = second;
             second->next = temp1;
