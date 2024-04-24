b8854513f74fad8c69c2c84b66797e4b31a0e837
Kerwin Tsai <kerwintsai@pm.me>
2024-04-24 17:30:17 +0800

    Finish 102

---
+++ problems/0102-binary-tree-level-order-traversal/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        vector<vector<int>> ans;
+        DFS(root, 0, ans);
+        return ans;
+    }
+
+private:
+    void DFS(TreeNode *root, int depth, vector<vector<int>> &ans)
+    {
+        if (!root)
+            return;
+        while (ans.size() <= depth)
+            ans.push_back({});
+        ans[depth].push_back(root->val);
+        DFS(root->left, depth + 1, ans);
+        DFS(root->right, depth + 1, ans);
+    }
+};
