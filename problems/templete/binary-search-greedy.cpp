2bd7d79ec2723ad66b699f78ea1ad0941a36b0af
Kerwin Tsai <kerwintsai@pm.me>
2024-06-14 21:20:12 +0800

    Add some templates

---
+++ problems/templete/binary-search-greedy.cpp
@@ -0,0 +1,46 @@
+int fn(vector<int>& arr) {
+    int left = MINIMUM_POSSIBLE_ANSWER;
+    int right = MAXIMUM_POSSIBLE_ANSWER;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (check(mid)) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
+
+bool check(int x) {
+    // this function is implemented depending on the problem
+    return BOOLEAN;
+}
+
+
+
+/* 
+ *
+ * If looking for a max;
+ *
+ */
+int fn(vector<int>& arr) {
+    int left = MINIMUM_POSSIBLE_ANSWER;
+    int right = MAXIMUM_POSSIBLE_ANSWER;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (check(mid)) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return right;
+}
+
+bool check(int x) {
+    // this function is implemented depending on the problem
+    return BOOLEAN;
+}
---
+++ problems/templete/monotonic-increasing-stack.cpp
@@ -0,0 +1,14 @@
+int fn(vector<int>& arr) {
+    stack<integer> stack;
+    int ans = 0;
+
+    for (int num: arr) {
+        // for monotonic decreasing, just flip the > to <
+        while (!stack.empty() && stack.top() > num) {
+            // do logic
+            stack.pop();
+        }
+
+        stack.push(num);
+    }
+}
