02fd2fdee5cc3988d7061b73ec851a8ee3f5e9f8
Kerwin Tsai <kerwintsai@pm.me>
2024-07-12 13:05:24 +0800

    Finish 238 Product of Array Except Self

---
+++ problems/0238-product-of-array-except-self/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size());
+
+        ans[0] = 1;
+        for(int i = 1; i < nums.size(); i++){
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+
+        int suffixProduct = 1;
+        for(int i = nums.size() - 1; i >= 0; i--){
+            ans[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+
+        return ans;
+    }
+};
